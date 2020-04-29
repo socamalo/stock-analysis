66cc62db982802d04cf60efb2933d8519051f501
Daniel Ding <4518369@qq.com>
2020-04-29 23:29:02 +0800

    optimized history K, pe_pb functions

--- stock class.py
+++ stock class.py
@@ -1,4 +1,4 @@
-import time
+import time, datetime
 import pandas as pd
 import baostock as bs
 
@@ -12,9 +12,9 @@ class stock:
         self.start_date = '2019-01-01'
         self.end_date = (time.strftime("%Y-%m-%d", time.localtime()))
 
+    @property
     def get_history_k(self):
-        stock_id = self.id
-        rs = bs.query_history_k_data_plus(stock_id,
+        rs = bs.query_history_k_data_plus(self.id,
                                           "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST",
                                           start_date=self.start_date, end_date=self.end_date,
                                           frequency="d", adjustflag="2")
@@ -23,13 +23,25 @@ class stock:
             # 获取一条记录，将记录合并在一起
             data_list.append(rs.get_row_data())
         result = pd.DataFrame(data_list, columns=rs.fields)
+        cols = ['open', 'close', 'low', 'high', 'preclose', 'volume', 'amount', 'turn', 'pctChg'] #多列转化成数字
+        result[cols] = result[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+        result['MA200'] = result.high.rolling(200).mean() #计算好200日移动平均
+        self.last_close_price = result['close'][len(result)-1] #初始化最后一个交易日收盘价
+        self.aboveMA200_mark = self.last_close_price > result['MA200'][len(result)-1]#初始化最后交易日收盘价是否在MA300之上
+
         return result
 
+    @property
     def get_pe_pb(self):
+        now = datetime.datetime.now()
+        delta = datetime.timedelta(days=-90)
+        pe_start_time = (now + delta).strftime('%Y-%m-%d')
+        # pe_start_time = (time.strftime("%Y-%m-%d", temp_time))
+        #避免获取太久前的数据，仅获取过去90天的数据。需要注意时间的格式。
         stock_id = self.id
         rs = bs.query_history_k_data_plus(stock_id,
                                           "date,code,close,peTTM,pbMRQ,psTTM,pcfNcfTTM",
-                                          start_date=self.start_date, end_date=self.end_date,
+                                          start_date=pe_start_time, end_date=self.end_date,
                                           frequency="d", adjustflag="3")
 
         #### 打印结果集 ####
@@ -38,6 +50,11 @@ class stock:
             # 获取一条记录，将记录合并在一起
             result_list.append(rs.get_row_data())
         result = pd.DataFrame(result_list, columns=rs.fields)
+        cols = ['peTTM', 'pbMRQ']  # 多列转化成数字
+        result[cols] = result[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+        self.peTTM = result['peTTM'][len(result)-1]
+        self.pbMRQ = result['pbMRQ'][len(result)-1]# MRQ: most recent quarter
+
         return result
 
     @property
