52edad9cebca0046bff71a227060658494d1f925
Daniel Ding <4518369@qq.com>
2020-05-02 22:09:47 +0800

    starting work on the main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+import time, datetime
+import pandas as pd
+import numpy as np
+import baostock as bs
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from Company_class import Company
